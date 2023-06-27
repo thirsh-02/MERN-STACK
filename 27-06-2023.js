7c2566596a80c0be736c00d3f93184a35b8658bc
THIRSHA A M <126067644+thirsh-02@users.noreply.github.com>
2023-06-27 16:03:10 +0530

    Update 27-06-2023.js

--- 27-06-2023.js
+++ 27-06-2023.js
@@ -17,3 +17,29 @@ app.get("/",(req,res)=>{                         //get is a mathod call() ,every
 app.listen(8080, ()=>{
     console.log("Server connector");
 });
+
+
+let express=require ("express")
+let app=express();
+app.get("sece",(req,res)=>{
+    res.send("This is get call");
+});
+
+app.post("/",(req,res)=>{
+    res.send("This is get call");
+});
+
+app.patch("/",(req,res)=>{
+    res.send("This is get call");
+});
+
+app.delete("/",(req,res)=>{
+    res.send("This is get call");
+});
+
+app.put("/",(req,res)=>{
+    res.send("This is get call");
+});
+app.listen(8080, ()=>{
+    console.log("Server connector");
+});
